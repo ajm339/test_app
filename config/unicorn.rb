28730d67ec479de2d06f373dd114d38e2d197b2c
Alex Meyers <ajm339@cornell.edu>
2014-06-07 13:17:36 -0700

    Adding Unicorn Configuration

---
+++ config/unicorn.rb
@@ -0,0 +1,38 @@
+#https://devcenter.heroku.com/articles/rails-unicorn (All except other URL sections)
+worker_processes Integer(ENV["WEB_CONCURRENCY"] || 3)
+timeout 15
+preload_app true
+
+before_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn master intercepting TERM and sending myself QUIT instead'
+    Process.kill 'QUIT', Process.pid
+  end
+
+  if defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  #https://www.digitalocean.com/community/articles/how-to-optimize-unicorn-workers-in-a-ruby-on-rails-app
+  if defined?(Resque)
+    Resque.redis.quit
+    Rails.logger.info('Disconnected from Redis')
+  end
+
+end
+
+after_fork do |server, worker|
+  Signal.trap 'TERM' do
+    puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
+  end
+
+  if defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+  end
+
+  #https://www.digitalocean.com/community/articles/how-to-optimize-unicorn-workers-in-a-ruby-on-rails-app
+  if defined?(Resque)
+    Resque.redis = ENV['REDIS_URI']
+    Rails.logger.info('Connected to Redis')
+  end
+end
